9832b9a229026bb2a02231513616ea39a6495442
uttamkajavadra <uttamkajavadra1966@gmail.com>
2025-02-13 11:07:07 +0530

    uplaod sponser ads button added

--- lib/widgets/common_bottom_bar.dart
+++ lib/widgets/common_bottom_bar.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:his_quiz/config/common_colors.dart';
+import 'package:his_quiz/config/common_text_style.dart';
 import 'package:his_quiz/config/image_path.dart';
+import 'package:his_quiz/globals.dart';
 
 commonBottomBar() {
-  return Column(
-    mainAxisSize: MainAxisSize.min,
+  return Stack(
+    // mainAxisSize: MainAxisSize.min,
+    alignment: Alignment.center,
     children: [
       Container(
         height: 70,
         color: CommonColors.primary,
+        margin: EdgeInsets.only(top: (Global.role.toString() == "admin")?20:0),
         child: Row(
           children: [
             Image.asset(
@@ -19,7 +23,9 @@ commonBottomBar() {
             Expanded(
                 child: Padding(
               padding: const EdgeInsets.all(12.0),
-              child: Image.asset(ImagePath.hisQuizFooter),
+              child: (Global.role.toString() != "admin")
+                ?Image.asset(ImagePath.hisQuizFooter)
+                : const SizedBox(),
             )),
             Image.asset(
               ImagePath.powerBY,
@@ -28,6 +34,17 @@ commonBottomBar() {
           ],
         ),
       ),
+      if(Global.role.toString() == "admin") Positioned(
+        top: 0,
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+          decoration: BoxDecoration(
+            color: CommonColors.primary,
+            borderRadius: BorderRadius.circular(8)
+          ),
+          child: Text("Upload sponsor ads", style: CommonTextStyle.button.copyWith(color: CommonColors.whiteColor, decoration: TextDecoration.underline, decorationColor: CommonColors.whiteColor),),
+        ),
+      )
     ],
   );
 }
